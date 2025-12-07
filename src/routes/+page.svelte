78cb0fe6e11a1b476880dd9f56082da13fb8f534
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2025-12-07 09:30:14 +0530

    remove text-selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <main
-    class="h-screen flex justify-center items-center text-white rounded-lg bg-neutral-400 opacity-50"
+    class="h-screen flex justify-center items-center text-white rounded-lg bg-neutral-400 opacity-50 select-none"
 >
     hi
 </main>
